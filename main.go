51a96d9fc16c954cea81f4ed303467480e945e06
HuangDongliu-DL <g280417484g>
2022-02-12 14:28:08 +0800

    增加权限验证功能

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"PicTool/ReadImage"
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -20,39 +21,41 @@ type RecvData struct {
 	Path   string `json:"path"`
 }
 
+type Config struct {
+	Host     string `json:"host"`
+	Album    string `json:"album"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func main() {
-	url := "https://blog.dongliu.site/img/upload"
-	//url = "http://localhost:8000/img/upload"
-	var CSRFToken string
+	// 读取配置文件
+	var config Config
+	tem, err := ioutil.ReadFile("./config.json")
+	checkErr(err)
+	err = json.Unmarshal(tem, &config)
+	checkErr(err)
+
+	uploadURL := config.Host + "/img/upload"
+	loginURL := config.Host + "/admin/login/?next=/admin/"
 	// 构造Session保证Cookie连续
 	jar, _ := cookiejar.New(nil)
 	client := &http.Client{
 		Jar: jar,
 	}
-	// 开始发送GET请求获取CSRT token
-	req, _ := http.NewRequest("GET", url, nil)
-	resp, err := client.Do(req)
+	// 模拟登入
+	_ = config.login(loginURL, client)
+
+	err, CSRFToken := getCSRFToken(uploadURL, client)
 	checkErr(err)
 
-	if resp.StatusCode != 200 {
-		fmt.Printf("%s\n", resp.Body)
-	} else {
-		for _, cooke := range resp.Cookies() {
-			if cooke.Name == "csrftoken" {
-				CSRFToken = cooke.Value
-			}
-		}
-	}
-	_ = resp.Close
-
-	imageAlbum := "1"
 	for _, p := range os.Args[1:] {
 		// 获取文件名称
 		imageName := ReadImage.GetImageName(p)
 		// 构建数据
 		formData := map[string]io.Reader{
 			"name":                strings.NewReader(imageName),
-			"album":               strings.NewReader(imageAlbum),
+			"album":               strings.NewReader(config.Album),
 			"csrfmiddlewaretoken": strings.NewReader(CSRFToken),
 		}
 		// 读取数据
@@ -60,12 +63,14 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		b, contentType := structureMultiform(formData, image, imageName)
+		b, contentType := structureMultiform(formData, map[string]ReadImage.Image{
+			imageName: image,
+		})
 		// 关闭文件
 		_ = image.Close()
 
 		// 发送数据
-		req, _ = http.NewRequest("POST", url, &b)
+		req, _ := http.NewRequest("POST", uploadURL, &b)
 		req.Header.Set("Content-Type", contentType)
 		res, err := client.Do(req)
 		checkErr(err)
@@ -86,8 +91,48 @@ func main() {
 	}
 }
 
+func (c Config) login(url string, client *http.Client) bool {
+	err, CSRFToken := getCSRFToken(url, client)
+	checkErr(err)
+	loginData := map[string]io.Reader{
+		"username":            strings.NewReader(c.Username),
+		"password":            strings.NewReader(c.Password),
+		"csrfmiddlewaretoken": strings.NewReader(CSRFToken),
+	}
+	b, contentType := structureMultiform(loginData, nil)
+	// 发送数据
+	req, _ := http.NewRequest("POST", url, &b)
+	req.Header.Set("Content-Type", contentType)
+	res, err := client.Do(req)
+	checkErr(err)
+	return res.StatusCode != http.StatusOK
+}
+
+// 获取CSRFToken
+func getCSRFToken(url string, client *http.Client) (error, string) {
+	CSRFToken := ""
+	// 开始发送GET请求获取CSRT token
+	req, _ := http.NewRequest("GET", url, nil)
+	resp, err := client.Do(req)
+	if err != nil {
+		return err, CSRFToken
+	}
+
+	if resp.StatusCode != 200 {
+		return errors.New("response status code is not 200"), CSRFToken
+	} else {
+		for _, cooke := range resp.Cookies() {
+			if cooke.Name == "csrftoken" {
+				CSRFToken = cooke.Value
+			}
+		}
+	}
+	_ = resp.Close
+	return nil, CSRFToken
+}
+
 // 构造多表单
-func structureMultiform(formData map[string]io.Reader, file ReadImage.Image, fileName string) (bytes.Buffer, string) {
+func structureMultiform(formData map[string]io.Reader, formFile map[string]ReadImage.Image) (bytes.Buffer, string) {
 	var b bytes.Buffer
 	w := multipart.NewWriter(&b)
 
@@ -99,10 +144,13 @@ func structureMultiform(formData map[string]io.Reader, file ReadImage.Image, fil
 		_, err = io.Copy(fw, r)
 		checkErr(err)
 	}
+	// 写入文件数据
+	for name, file := range formFile {
+		fw, err := w.CreateFormFile("image", name)
+		_, err = io.Copy(fw, file)
+		checkErr(err)
+	}
 
-	fw, err := w.CreateFormFile("image", fileName)
-	_, err = io.Copy(fw, file)
-	checkErr(err)
 	contentType := w.FormDataContentType()
 
 	// 关闭 multipart writer
